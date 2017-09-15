b4cf3579569a1b68dc2068be744a9eaafe5c3982
meirinder <s.kulizhnikov@gmail.com>
2017-09-15 12:12:42 +0700

    Release 1.0

--- CurrencyConverter/HelpTableViewCell.swift
+++ CurrencyConverter/HelpTableViewCell.swift
@@ -13,15 +13,10 @@ class HelpTableViewCell: UITableViewCell {
     @IBOutlet var lableCharCode: UILabel!
     @IBOutlet var lableName: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
 
 }
--- CurrencyConverter/HelpTableViewController.swift
+++ CurrencyConverter/HelpTableViewController.swift
@@ -11,7 +11,6 @@ import SwiftyJSON
 
 class HelpTableViewController: UITableViewController {
 
- //   @IBOutlet var itemCell: UITableViewCell!
     
     
     var allCharCodes: [String] = []
@@ -20,54 +19,9 @@ class HelpTableViewController: UITableViewController {
   
     override func viewDidLoad() {
         super.viewDidLoad()
-     //   getCurseOnDay()
-        
-      
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
     
     
-   /* func getCurseOnDay(){
-        let url = URL(string: "https://www.cbr-xml-daily.ru/daily_json.js")
-        let task = URLSession.shared.dataTask(with: url! as URL) { data, response, error in
-            
-            guard let data = data, error == nil else { return }
-            
-            let json = JSON(data: data)
-            
-            print(NSString(data: data, encoding: String.Encoding.utf8.rawValue)!)
-            
-            let instanse = Item(charCode: "RUR", id: "R00000", numCode: 643, name: "Российский рубль", value: 1, previous: 1, nominal: 1)
-            self.itemStore["RUR"] = instanse
-            
-            
-            for i in 1...json["Valute"].count
-            {
-                let instanse = Item(charCode: json["Valute"][self.allCharCodes[i]]["CharCode"].string!,
-                                    id: json["Valute"][self.allCharCodes[i]]["ID"].string!,
-                                    numCode: json["Valute"][self.allCharCodes[i]]["NumCode"].intValue,
-                                    name: json["Valute"][self.allCharCodes[i]]["Name"].string!,
-                                    value: json["Valute"][self.allCharCodes[i]]["Value"].doubleValue,
-                                    previous: json["Valute"][self.allCharCodes[i]]["Previous"].int!,
-                                    nominal:json["Valute"][self.allCharCodes[i]]["Nominal"].doubleValue )
-                
-                self.itemStore[self.allCharCodes[i]] = instanse
-            }
-        }
-        
-        task.resume()
-    }*/
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
